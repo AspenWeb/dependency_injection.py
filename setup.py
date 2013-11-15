fba00d55f2c67d77f676a8cca5c5d5e95275957a
Chad Whitacre <chad@zetaweb.com>
2013-11-15 07:46:20 -0500

    Reformat setup.py

--- setup.py
+++ setup.py
@@ -10,18 +10,17 @@ setup( name='dependency_injection'
      , url='https://dependency-injection-py.readthedocs.org'
      , version='0.0.0-dev'
      , py_modules=['dependency_injection']
-     , classifiers=[
-         'Development Status :: 5 - Production/Stable',
-         'Intended Audience :: Developers',
-         'License :: CC0 1.0 Universal (CC0 1.0) Public Domain Dedication',
-         'Operating System :: OS Independent',
-         'Programming Language :: Python :: 2',
-         'Programming Language :: Python :: 2.6',
-         'Programming Language :: Python :: 2.7',
-         'Programming Language :: Python :: 3',
-         'Programming Language :: Python :: 3.2',
-         'Programming Language :: Python :: 3.3',
-         'Topic :: Database :: Front-Ends',
-         'Topic :: Software Development :: Libraries :: Python Modules',
-       ],
+     , classifiers=[ 'Development Status :: 5 - Production/Stable'
+                   , 'Intended Audience :: Developers'
+                   , 'License :: CC0 1.0 Universal (CC0 1.0) Public Domain Dedication'
+                   , 'Operating System :: OS Independent'
+                   , 'Programming Language :: Python :: 2'
+                   , 'Programming Language :: Python :: 2.6'
+                   , 'Programming Language :: Python :: 2.7'
+                   , 'Programming Language :: Python :: 3'
+                   , 'Programming Language :: Python :: 3.2'
+                   , 'Programming Language :: Python :: 3.3'
+                   , 'Topic :: Database :: Front-Ends'
+                   , 'Topic :: Software Development :: Libraries :: Python Modules'
+                    ]
       )
