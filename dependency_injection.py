ef43225c4348a1cdad77cfc1cb23172beb64dd5e
Chad Whitacre <chad@zetaweb.com>
2013-12-18 12:47:51 -0500

    Bump version to 1.0.1

--- dependency_injection.py
+++ dependency_injection.py
@@ -50,7 +50,7 @@ from __future__ import absolute_import, division, print_function, unicode_litera
 from collections import namedtuple
 
 
-__version__ = '1.0.0-dev'
+__version__ = '1.0.1'
 
 
 def resolve_dependencies(function, available):
